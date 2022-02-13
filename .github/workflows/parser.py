99f66140bc43ac7db5ac48d278efd1a235688920
Seff <46768740+seffs@users.noreply.github.com>
2022-02-13 01:24:18 +0100

    Temp rollback to saturday

--- .github/workflows/parser.py
+++ .github/workflows/parser.py
@@ -5,11 +5,11 @@ import os
 import json
 import re
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import pdfplumber
 
 DATAPATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep + "data_BW" + os.sep
-DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d')
+DATE_STR = datetime.fromtimestamp((datetime.now() - timedelta(days=1)).timestamp()).strftime('%Y-%m-%d')
 PDF_FILENAME = "BW_{}.pdf".format(DATE_STR)
 JSON_FILENAME = "BW_{}.json".format(DATE_STR)
 PDF_FULLNAME = DATAPATH + PDF_FILENAME
